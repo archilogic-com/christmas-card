24ad33b83def06224476ae097c8824b66b82fc8d
Martin N. <mr.avgp@googlemail.com>
2014-11-14 17:56:29 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,126 @@
+var WIDTH = document.body.clientWidth, HEIGHT = document.body.clientHeight;
+var scene = new THREE.Scene(),
+    camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000),
+    renderer = new THREE.WebGLRenderer();
+
+document.body.appendChild(renderer.domElement);
+
+// React to silly browsers...
+WIDTH = document.body.clientWidth;
+HEIGHT = document.body.clientHeight;
+renderer.setSize(WIDTH, HEIGHT);
+
+// Setup ligthing
+
+var ambient = new THREE.AmbientLight(0x404040);
+scene.add(ambient);
+
+light = new THREE.DirectionalLight(0xffffff);
+
+light.position.set(0, 100, 60);
+light.castShadow = true;
+light.shadowCameraLeft = -60;
+light.shadowCameraTop = -60;
+light.shadowCameraRight = 60;
+light.shadowCameraBottom = 60;
+light.shadowCameraNear = 1;
+light.shadowCameraFar = 1000;
+light.shadowBias = -.0001
+light.shadowMapWidth = light.shadowMapHeight = 1024;
+light.shadowDarkness = .7;
+scene.add(light);
+
+// Add a random cube...
+
+var geometry = new THREE.CubeGeometry(1, 1, 1);
+var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+cube.position.set(10, 1, -20);
+scene.add(cube);
+
+// Create the glassy box
+
+var box = new THREE.Mesh(
+  new THREE.CubeGeometry(50, 50, 50),
+  new THREE.MeshLambertMaterial({
+    color: 0x80aaff,
+    reflectivity: .8,
+    transparent: true,
+    opacity: 0.5,
+    side: THREE.DoubleSide
+  })
+);
+
+//box.add(camera);
+scene.add(box);
+
+// create ground
+
+var groundGeometry = new THREE.CubeGeometry(50, 5, 50);
+var groundMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide});
+var ground = new THREE.Mesh(groundGeometry,groundMaterial);
+ground.position.set(0, -22.5, 0);
+box.add(ground);
+//scene.add(ground);
+
+// create Snow
+
+var NUM_SNOWFLAKES = 10000;
+snowGeometry = new THREE.Geometry();
+snowMaterial = new THREE.PointCloudMaterial({ color: 0xffffff, size: 0.1 });
+var snow = new THREE.PointCloud(snowGeometry, snowMaterial);
+snow.sortParticles = true;
+
+for(var i=0;i<NUM_SNOWFLAKES;i++) {
+	snowGeometry.vertices.push(new THREE.Vector3(-25 + Math.random() * 50, -25 + Math.random() * 50, -25 + Math.random() * 50));
+}
+
+var spin = 0;
+
+//scene.add(snow);
+box.add(snow);
+
+// Position camera
+
+camera.position.set(0,25,100);
+
+// Event listeners
+var dragStart = {x: 0, y: 0, dragging: false};
+window.addEventListener('mousedown', function(e) {
+  dragStart.x = e.clientX;
+  dragStart.y = e.clientY;
+  dragStart.dragging = true;
+});
+
+window.addEventListener('mousemove', function(e) {
+  if(!dragStart.dragging) return;
+  if(e.clientX < dragStart.x)
+    box.rotation.y -= 0.05;
+  else
+    box.rotation.y += 0.05;
+});
+
+window.addEventListener('mouseup', function(e) {
+  dragStart.dragging = false;
+  spin = (e.clientX - dragStart.x) / 100;
+});
+// Go!
+
+function render() {
+	requestAnimationFrame(render);
+
+  for(var i=0;i<NUM_SNOWFLAKES;i++) {
+		snowGeometry.vertices[i].y -= 0.05;
+    snowGeometry.vertices[i].x += spin;
+
+    spin = 0;
+
+		if (snowGeometry.vertices[i].y < -25) {
+		snowGeometry.vertices[i].y = 10 + Math.random() * 15;
+		}
+	}
+
+	renderer.render(scene, camera);
+}
+
+render();
