14e7f08438734b61430c49e4fd2aba6c234651e1
Martin Naumann <martin@geekonaut.de>
2014-12-01 23:28:59 +0100

    Playing with the lights

--- main.js
+++ main.js
@@ -34,7 +34,6 @@ var ambient = new THREE.AmbientLight(0xffffff);
 scene.add(ambient);
 
 light = new THREE.DirectionalLight(0xffffff);
-
 light.position.set(0, 100, 60);
 light.castShadow = true;
 light.shadowCameraLeft = -60;
@@ -46,7 +45,23 @@ light.shadowCameraFar = 1000;
 light.shadowBias = -.0001
 light.shadowMapWidth = light.shadowMapHeight = 1024;
 light.shadowDarkness = .7;
+light.intensity = 2;
 scene.add(light);
+
+lightBelow = new THREE.DirectionalLight(0xffffff);
+lightBelow.position.set(0, -50, 0);
+lightBelow.castShadow = true;
+lightBelow.shadowCameraLeft = -60;
+lightBelow.shadowCameraTop = -60;
+lightBelow.shadowCameraRight = 60;
+lightBelow.shadowCameraBottom = 60;
+lightBelow.shadowCameraNear = 1;
+lightBelow.shadowCameraFar = 1000;
+lightBelow.shadowBias = -.0001
+lightBelow.shadowMapWidth = light.shadowMapHeight = 1024;
+lightBelow.shadowDarkness = .7;
+lightBelow.rotation.set(-Math.PI, 0, 0);
+scene.add(lightBelow);
 
 // Create the center
 
@@ -69,7 +84,7 @@ loader.load("model/XMASCard9_TextureImplementation.obj", "model/XMASCard9_Textur
   document.body.removeChild(document.getElementById("loading"));
 });
 
-// create Snow
+// Create Snow
 
 var snowGeometry = new THREE.Geometry(),
     snowflake    = THREE.ImageUtils.loadTexture((window.location.hash.slice(1) || "snowflake") + ".png"),
@@ -127,25 +142,6 @@ hammertime.on('pinchmove', function(e) {
 });
 
 /*
-window.addEventListener('mousedown', function(e) {
-  dragStart = e.clientX;
-  dragging = true;
-});
-
-window.addEventListener('mousemove', function(e) {
-  if(!dragging) return;
-  if(e.clientX < dragStart && spin > -WIDTH/10)
-    spin -= 50;
-  else if(e.clientX > dragStart && spin < WIDTH/10)
-    spin += 50;
-
-  dragStart = e.clientX;
-});
-
-window.addEventListener('mouseup', function(e) {
-  dragging = false;
-});
-
 window.addEventListener('devicemotion', function(e) {
   if(e.rotationRate === null || e.rotationRate.alpha === null) return;
   spin += Math.round(e.rotationRate.alpha * -150);
