e793c1bbdee9d8f8d5ecda69efb9b09d7921b62d
Martin Naumann <martin@geekonaut.de>
2014-12-15 12:18:21 +0100

    Snow is now bundled as a module

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 var LoadingScreen = require('./loading.js'),
+    Snow = require('./snow.js'),
     World = require('three-world'),
     THREE = require('three'),
     MTLLoader = require('./loaders/MTLLoader.js'),
     OBJMTLLoader = require('./loaders/OBJMTLLoader.js');
-
-var NUM_SNOWFLAKES = 2000,
-    FALL_SPEED_QUOTIENT = 45;
 
 var spin = 0,
     wasMoved = false;
@@ -71,34 +69,8 @@ loader.load("model/XMASCard9_TextureImplementation.obj", "model/XMASCard9_Textur
   World.startRenderLoop();
 });
 
-// Create Snow
-
-var snowGeometry = new THREE.Geometry(),
-    snowflake    = THREE.ImageUtils.loadTexture((window.location.hash.slice(1) || "snowflake") + ".png"),
-    snowMaterial = new THREE.PointCloudMaterial({
-      size: 0.3,
-      map: snowflake,
-      opacity: 0.5,
-      depthTest: false,
-      blending: THREE.AdditiveBlending,
-      transparent: true
-    }),
-    snow         = new THREE.PointCloud(snowGeometry, snowMaterial);
-
-snow.sortParticles = true;
-
-for(var i=0;i<NUM_SNOWFLAKES;i++) {
-  var vector = new THREE.Vector3(-25 + Math.random() * 50, -25 + Math.random() * 50, -25 + Math.random() * 50);
-  vector.velocity = {
-    x: -1 + Math.random() * 2,
-    y: Math.random() / FALL_SPEED_QUOTIENT,
-    z: -1 + Math.random() * 2,
-    recalcIn: Math.round(Math.random() * 10)
-  };
-	snowGeometry.vertices.push(vector);
-}
-
-centerAnchor.add(snow);
+Snow.init((window.location.hash.slice(1) || "snowflake") + ".png");
+centerAnchor.add(Snow.getObject());
 
 // Creating skybox
 var directions  = ["right", "left", "top", "top", "front", "back"];
@@ -185,46 +157,7 @@ function render() {
 
   // Snow movement
 
-  for(var i=0;i<NUM_SNOWFLAKES;i++) {
-		snowGeometry.vertices[i].y -= snowGeometry.vertices[i].velocity.y;
-    snowGeometry.vertices[i].x += snowGeometry.vertices[i].velocity.x / 100;
-    snowGeometry.vertices[i].z += snowGeometry.vertices[i].velocity.z / 100;
-
-    // Boundaries
-
-    if(snowGeometry.vertices[i].x < -25) snowGeometry.vertices[i].x =  25;
-    if(snowGeometry.vertices[i].x >  25) snowGeometry.vertices[i].x = -25;
-
-    if(snowGeometry.vertices[i].z < -25) snowGeometry.vertices[i].z =  25;
-    if(snowGeometry.vertices[i].z >  25) snowGeometry.vertices[i].z = -25;
-
-    // Velocity recalc
-
-    if(--snowGeometry.vertices[i].velocity.recalcIn == 0) {
-      snowGeometry.vertices[i].velocity = {
-        x: -1 + Math.random() * 2,
-        y: Math.random() / FALL_SPEED_QUOTIENT,
-        z: -1 + Math.random() * 2,
-        recalcIn: Math.round(Math.random() * 10)
-      };
-    }
-
-    // Reset
-
-	if (snowGeometry.vertices[i].y < -25) {
-      snowGeometry.vertices[i].x = -25 + Math.random() * 50;
-		  snowGeometry.vertices[i].y =  10 + Math.random() * 15;
-      snowGeometry.vertices[i].z = -25 + Math.random() * 50;
-
-      snowGeometry.vertices[i].velocity = {
-        x: -1 + Math.random() * 2,
-        y: Math.random() / FALL_SPEED_QUOTIENT,
-        z: -1 + Math.random() * 2,
-        recalcIn: Math.round(Math.random() * 10)
-      };
-
-    }
-  }
+  Snow.update();
 
   // Automatic camera rotation
 
---
+++ js/snow.js
@@ -0,0 +1,91 @@
+var THREE = require('three');
+
+module.exports = (function(){
+  var Snow = {};
+
+  var NUM_SNOWFLAKES = 2000,
+      FALL_SPEED_QUOTIENT = 45;
+
+  var snow, snowGeometry;
+
+  // Create Snow
+
+  Snow.init = function(flakeImgUrl, numFlakes, fallSpeedQuotient) {
+    if(numFlakes) NUM_SNOWFLAKES = numFlakes;
+    if(fallSpeedQuotient) FALL_SPEED_QUOTIENT = fallSpeedQuotient;
+
+    var snowflake    = THREE.ImageUtils.loadTexture(flakeImgUrl),
+        snowMaterial = new THREE.PointCloudMaterial({
+          size: 0.3,
+          map: snowflake,
+          opacity: 0.5,
+          depthTest: false,
+          blending: THREE.AdditiveBlending,
+          transparent: true
+        });
+
+    snowGeometry = new THREE.Geometry(),
+    snow = new THREE.PointCloud(snowGeometry, snowMaterial);
+
+    snow.sortParticles = true;
+
+    for(var i=0;i<NUM_SNOWFLAKES;i++) {
+      var vector = new THREE.Vector3(-25 + Math.random() * 50, -25 + Math.random() * 50, -25 + Math.random() * 50);
+      vector.velocity = {
+        x: -1 + Math.random() * 2,
+        y: Math.random() / FALL_SPEED_QUOTIENT,
+        z: -1 + Math.random() * 2,
+        recalcIn: Math.round(Math.random() * 10)
+      };
+      snowGeometry.vertices.push(vector);
+    }
+  };
+
+  Snow.getObject = function() { return snow; };
+
+  Snow.update = function() {
+    for(var i=0;i<NUM_SNOWFLAKES;i++) {
+      snowGeometry.vertices[i].y -= snowGeometry.vertices[i].velocity.y;
+      snowGeometry.vertices[i].x += snowGeometry.vertices[i].velocity.x / 100;
+      snowGeometry.vertices[i].z += snowGeometry.vertices[i].velocity.z / 100;
+
+      // Boundaries
+
+      if(snowGeometry.vertices[i].x < -25) snowGeometry.vertices[i].x =  25;
+      if(snowGeometry.vertices[i].x >  25) snowGeometry.vertices[i].x = -25;
+
+      if(snowGeometry.vertices[i].z < -25) snowGeometry.vertices[i].z =  25;
+      if(snowGeometry.vertices[i].z >  25) snowGeometry.vertices[i].z = -25;
+
+      // Velocity recalc
+
+      if(--snowGeometry.vertices[i].velocity.recalcIn == 0) {
+        snowGeometry.vertices[i].velocity = {
+          x: -1 + Math.random() * 2,
+          y: Math.random() / FALL_SPEED_QUOTIENT,
+          z: -1 + Math.random() * 2,
+          recalcIn: Math.round(Math.random() * 10)
+        };
+      }
+
+      // Reset
+
+    if (snowGeometry.vertices[i].y < -25) {
+        snowGeometry.vertices[i].x = -25 + Math.random() * 50;
+        snowGeometry.vertices[i].y =  10 + Math.random() * 15;
+        snowGeometry.vertices[i].z = -25 + Math.random() * 50;
+
+        snowGeometry.vertices[i].velocity = {
+          x: -1 + Math.random() * 2,
+          y: Math.random() / FALL_SPEED_QUOTIENT,
+          z: -1 + Math.random() * 2,
+          recalcIn: Math.round(Math.random() * 10)
+        };
+
+      }
+    }
+
+  }
+
+  return Snow;
+})()
