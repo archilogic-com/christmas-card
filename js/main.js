294ca73dcc81103003111a80e1f5bc32d708c82a
Martin Naumann <martin@geekonaut.de>
2014-12-15 18:19:25 +0100

    bunch o' fixes

--- js/main.js
+++ js/main.js
@@ -5,33 +5,17 @@ var LoadingScreen = require('./loading.js'),
     KineticControls = require('./kinetic-controls.js'),
     THREE = require('three'),
     loadAL2 = require('./loaders/al2loader');
-//    MTLLoader = require('./loaders/MTLLoader.js'),
-//    OBJMTLLoader = require('./loaders/OBJMTLLoader.js');
 
-//var loader = new OBJMTLLoader();
 LoadingScreen.start(document.getElementById("loading"));
 
 World.init({
-  farPlane: 5000,
-  renderCallback: render
+  farPlane: 10000,
+  renderCallback: render,
+  clearColor: 0x00001f,
+  ambientLightColor: 0
 });
 
 // Setup ligthing
-
-light = new THREE.DirectionalLight(0xffffff);
-light.position.set(0, 100, 60);
-light.castShadow = true;
-light.shadowCameraLeft = -60;
-light.shadowCameraTop = -60;
-light.shadowCameraRight = 60;
-light.shadowCameraBottom = 60;
-light.shadowCameraNear = 1;
-light.shadowCameraFar = 1000;
-light.shadowBias = -.0001
-light.shadowMapWidth = light.shadowMapHeight = 1024;
-light.shadowDarkness = .7;
-//light.intensity = 1.6;
-World.add(light);
 
 lightBelow = new THREE.DirectionalLight(0xffffff);
 lightBelow.position.set(0, -50, 0);
@@ -43,10 +27,10 @@ lightBelow.shadowCameraBottom = 60;
 lightBelow.shadowCameraNear = 1;
 lightBelow.shadowCameraFar = 1000;
 lightBelow.shadowBias = -.0001
-lightBelow.shadowMapWidth = light.shadowMapHeight = 1024;
+lightBelow.shadowMapWidth = 1024;
 lightBelow.shadowDarkness = .7;
 lightBelow.rotation.set(-Math.PI, 0, 0);
-//lightBelow.intensity = 1.5;
+lightBelow.intensity = 0.2;
 World.add(lightBelow);
 
 // Create the center
@@ -71,38 +55,26 @@ loadAL2({
   }
 });
 
-/*
-loader.load("model/XMASCard9_TextureImplementation.obj", "model/XMASCard9_TextureImplementation.mtl", function(awesomeStuff) {
-  console.log("Loaded");
-
-  awesomeStuff.position.set(5, 3, -1);
-  awesomeStuff.scale.set(0.2, 0.2, 0.2)
-  centerAnchor.add(awesomeStuff);
-
-  LoadingScreen.stop();
-  World.startRenderLoop();
-});
-*/
-
-
-Snow.init((window.location.hash.slice(1) || "snowflake") + ".png");
+Snow.init((window.location.hash.slice(1) || "snowflake") + ".png", 6000);
 centerAnchor.add(Snow.getObject());
 
 var sky = Skybox('skymap/stars_', 'jpg');
 World.add(sky);
 
-
 // Position camera
 
 var camera = World.getCamera();
 
-camera.position.set(0, 10, 40);
+camera.position.set(0, 0, 40);
 camAnchor.rotation.order = 'YXZ';
 camAnchor.add(camera);
+window.camAnchor = camAnchor;
 World.add(camAnchor);
 
+camAnchor.rotation.set(-Math.PI/8, Math.PI/2 - 0.1, 0);
+
 // Event listeners
-KineticControls.init(camera, camAnchor);
+KineticControls.init(camera, camAnchor, 23);
 
 // Go!
 
@@ -116,5 +88,5 @@ function render() {
 
   // Automatic camera rotation
 
-  centerAnchor.rotation.y += 0.001;
+  if(!KineticControls.wasMoved()) centerAnchor.rotation.y += 0.001;
 }
